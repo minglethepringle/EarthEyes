71edaf7d8b1ca23f1326827b177be79fbda54390
Jet Chung <jetchung1@gmail.com>
2019-05-22 14:16:50 -0400

    javascript

--- JS/index.js
+++ JS/index.js
@@ -6,22 +6,28 @@ async function app() {
   // Load model
   net = await mobilenet.load();
 
+  var loading = true;
+
   var imgEl = document.getElementById('img');
   var result = await net.classify(imgEl);
-  
-  console.log(result);
   var guess = result[0];
   var prob = result[0]["probability"];
-  var className = result[0]["className"];
-  var validate = false;
+  
   console.log(guess);
   if (prob >= .4 ){ //reduce false positives
     validate = true;
-    //pass this to Earth911 API
+    //pass this to Earth911 APIs
     //add sending DIY video kek
     console.log("");
     document.getElementById("header").innerHTML=guess["className"];
-  }
+
+
+    var loading = false;
+    var huh = (document.getElementById("header").style.display);
+    if (huh == "none"){
+      console.log("woah");
+      document.getElementById("loading").style.display = "";
+    }
   else{
     //request user for their descriptions
     document.getElementById("header").innerHTML="wut is this";
@@ -29,5 +35,5 @@ async function app() {
   }
 
 }
-
+}
 app();
