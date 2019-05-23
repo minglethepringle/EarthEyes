46394ce4a3e7ea5fbaae1ab46c3941ffca366d2d
Jet Chung <jetchung1@gmail.com>
2019-05-22 22:27:18 -0400

    fixing js

--- JS/index.js
+++ JS/index.js
@@ -6,25 +6,33 @@ async function app() {
   // Load model
   net = await mobilenet.load();
 
-  var loading = true;
 
   var imgEl = document.getElementById('img');
   var result = await net.classify(imgEl);
   var guess = result[0];
   var prob = result[0]["probability"];
-  document.getElementById("header").textContent = "why so slow";
-
-  var headerStyle = document.getElementById("header").style.display;
-  console.log(headerStyle);
-  if (headerStyle != "none") {
-    document.getElementById("loading").style.display = "none";
-  } else if (headerStyle == "none") {
-    document.getElementById("loading").style.display = "";
-    console.log("here");
-  }
+  document.getElementById("header").textContent = guess["className"];
+  console.log(guess);
 
   //loading.io
 
 }
 
 app();
+
+var update_loop = setInterval(Main, 100);
+
+setTimeout(function () {
+  document.getElementById("header").style.display = ""
+}, 5000);
+
+Main();
+
+function Main() {
+  var headerStyle = document.getElementById("header").style.display;
+  if (headerStyle != "none") {
+    document.getElementById("loading").style.display = "none";
+  } else if (headerStyle == "none") {
+    document.getElementById("loading").style.display = "";
+  }
+}
