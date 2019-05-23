bb7726a07c2bc9617a060a3779716a9c36fd05e4
Jet Chung <jetchung1@gmail.com>
2019-05-22 22:24:16 -0400

    stash huh

--- JS/index.js
+++ JS/index.js
@@ -12,19 +12,18 @@ async function app() {
   var result = await net.classify(imgEl);
   var guess = result[0];
   var prob = result[0]["probability"];
-  document.getElementById("header").textContent="why so slow";
+  document.getElementById("header").textContent = "why so slow";
 
   var headerStyle = document.getElementById("header").style.display;
   console.log(headerStyle);
-  if (headerStyle != "none"){
+  if (headerStyle != "none") {
     document.getElementById("loading").style.display = "none";
-  }
-  else if (headerStyle == "none"){
+  } else if (headerStyle == "none") {
     document.getElementById("loading").style.display = "";
     console.log("here");
   }
 
-//loading.io
+  //loading.io
 
 }
 
