a8fdb6981c2fde419778d7bd7a77455548b3db69
Jet Chung <jetchung1@gmail.com>
2019-05-25 23:10:42 -0400

    map working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,9 @@
-$("body").ready(function() {
+var latitude;
+var longitude;
+var new_latitude;
+var new_longitude;
+var hide = false;
+$("body").ready(function () {
     // Mobilenet
     async function loadNet() {
         startLoading();
@@ -10,35 +15,55 @@ $("body").ready(function() {
 
         var imgEl = document.getElementById('photo');
         var result = await net.classify(imgEl);
-        if(result.length <= 0) {
+        if (result.length <= 0) {
             setResult("", "");
             stopLoading();
             return;
         }
         var guess = result[0]["className"];
         var probability = result[0]["probability"];
-        if(probability <= 0.05) {
+        if (probability <= 0.05) {
             setResult("", "");
             stopLoading();
             return;
         }
-    
-        var latitude = "", longitude = "";
-        navigator.geolocation.getCurrentPosition(function(pos) {
+
+        navigator.geolocation.getCurrentPosition(function (pos) {
             latitude = pos.coords.latitude;
             longitude = pos.coords.longitude;
-
+            console.log(latitude);
+            console.log(longitude);
             var query = guess;
             query = encodeURIComponent(query);
             console.log(query);
-            $.get("/endpoint/query/"+latitude+"/"+longitude+"/"+query, function(data){
+            $.get("/endpoint/query/" + latitude + "/" + longitude + "/" + query, function (data) {
+                new_latitude = data["latitude"];
+                new_longitude = data["longitude"];
+                console.log(new_latitude);
+                console.log(new_longitude);
+                //var hre = "https://www.google.com/maps/dir/?api=1&origin="+latitude+","+longitude+"&destination="+new_latitude+","+new_longitude;
+                //        var hre="https://www.google.com/maps/dir/"+latitude+","+longitude+"/"+new_latitude+","+new_longitude+"/"+new_latitude+","+new_longitude+"18z";
+                if (new_latitude && new_longitude && latitude && longitude) {
+                    console.log("here1");
+                    var hre1 = "https://www.google.com/maps/dir/?api=1&origin=" + latitude + "," + longitude + "&destination=" + new_latitude + "," + new_longitude;
+                    document.getElementById("woahwoahwoah").onclick = function () {
+                        window.open(hre1, '_blank', 'resizable=yes')
+                    };
+                    document.getElementById("woahwoahwoah").style.display = "";
+                } else {
+                    document.getElementById("woahwoahwoah").style.display = "none";
+
+                }
+
                 setResult(guess, (!$.isEmptyObject(data)) ? data.description : "");
                 stopLoading();
-            }).fail(function() {
+            }).fail(function () {
                 alert("Server error!");
                 stopLoading();
             });
+
         });
+
     }
 
     /* Video Streaming */
@@ -46,21 +71,27 @@ $("body").ready(function() {
     var canvas = $("#canvas")[0];
     var photo = $("#photo")[0];
     var streaming = false;
-    var width = 0, height = 0;
-    navigator.mediaDevices.getUserMedia({ video: {facingMode: 'environment'}, audio: false })
-        .then(function(stream) {
+    var width = 0,
+        height = 0;
+    navigator.mediaDevices.getUserMedia({
+            video: {
+                facingMode: 'environment'
+            },
+            audio: false
+        })
+        .then(function (stream) {
             video.srcObject = stream;
             video.play();
             streaming = true;
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log("An error occurred: " + err);
         });
-    video.addEventListener('canplay', function(ev){
+    video.addEventListener('canplay', function (ev) {
         width = video.videoWidth;
         height = video.videoHeight;
         if (!streaming) {
-            height = video.videoHeight / (video.videoWidth/width);
+            height = video.videoHeight / (video.videoWidth / width);
             video.setAttribute('width', width);
             video.setAttribute('height', height);
             canvas.setAttribute('width', width);
@@ -74,7 +105,7 @@ $("body").ready(function() {
             canvas.width = width;
             canvas.height = height;
             context.drawImage(video, 0, 0, width, height);
-            
+
             var data = canvas.toDataURL('image/jpeg');
             photo.setAttribute('src', data);
             photo.setAttribute('width', width);
@@ -89,16 +120,17 @@ $("body").ready(function() {
         }
     }
 
+
     function clearPhoto() {
         var context = canvas.getContext('2d');
         context.fillStyle = "#AAA";
         context.fillRect(0, 0, canvas.width, canvas.height);
-    
+
         var data = canvas.toDataURL('image/png');
         photo.setAttribute('src', data);
     }
 
-    $(".take-picture").click(function(e) {
+    $(".take-picture").click(function (e) {
         e.preventDefault();
         takePicture();
     });
@@ -114,19 +146,19 @@ $("body").ready(function() {
 
     /* Result section */
     function setResult(object, location) {
-        if(object == "") {
+        if (object == "") {
             $(".result-object p").hide();
-            $(".result-object-name").text("I'm not sure what this is.");    
+            $(".result-object-name").text("I'm not sure what this is.");
             $(".result hr").addClass("m-0");
             $('.result-location').hide();
-        } else if(location == "") {
+        } else if (location == "") {
             $(".result-object-name").text(object);
             $(".result-location-name").text("N/A");
         } else {
             $(".result-object-name").text(object);
             $(".result-location-name").text(location);
         }
-        
+
         showResultSection();
     }
 
@@ -144,28 +176,28 @@ $("body").ready(function() {
     /* ============================================ */
 
     /* Search Bar */
-    $(".form-control").click(function(){
+    $(".form-control").click(function () {
         $(this).focus();
     });
 
-    $(".search-form").submit(function(e) {
+    $(".search-form").submit(function (e) {
         e.preventDefault();
         var searchInput = $(".search-form input").val();
-        if(searchInput.length > 0) {
+        if (searchInput.length > 0) {
             searchInput = encodeURIComponent(searchInput);
             startLoading();
-            $.get("/endpoint/getMaterials/" +searchInput, function(data){
+            $.get("/endpoint/getMaterials/" + searchInput, function (data) {
                 stopLoading();
                 var results = data["results"];
-                if(results.length == 0) {
+                if (results.length == 0) {
                     alert("No results found!");
                     return;
                 }
                 $(".search-list").html("");
-                $.each(results, function(i, obj) {
+                $.each(results, function (i, obj) {
                     $(".search-list").append('<li data-id="' + obj.material_id + '">' + obj.description + '</li>');
                 });
-            }).fail(function() {
+            }).fail(function () {
                 alert("Could not search!");
                 stopLoading();
             });
