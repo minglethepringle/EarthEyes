c7c7adef2952303d28a593ca49449bc670525847
Jet Chung <jetchung1@gmail.com>
2019-05-25 21:44:27 -0400

    9:44

--- server.js
+++ server.js
@@ -1,70 +1,15 @@
+
+'use strict';
+
+// [START gae_node_request_example]
 const express = require('express');
-const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
-var https = require('https');
-/**
- * Starts the Express server.
- *
- * @return {ExpressServer} instance of the Express server.
- */
-function startServer() {
-    const app = express();
+//const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+const https = require('https');
+const app = express();
 
-    // Redirect HTTP to HTTPS
-    app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
-
-    // Handle requests for the data
-    app.get('/endpoint/countries', getCountries);
-    app.get('/endpoint/location', getLocations);
-    var util = require('util');
-    app.get("/endpoint/query/:latitude/:longitude/:query",function(req, res){
-        var latitude = req.params.latitude;
-        var longitude = req.params.longitude;
-        getMaterialsID(req.params.query, latitude, longitude, function(data) {
-            if (data["result"].length > 0){
-                res.send(data["result"][0]);
-            }
-            else{
-                console.log("stop trying to reuse 353");
-                res.send({});
-            }
-        });
-        // query = id;
-        // console.log(id);
-    });
-
-    // Handle requests for static files
-    app.use(express.static('public'));
-
-    // Start the server
-    return app.listen('8000', () => {
-        // eslint-disable-next-line no-console
-        console.log('Local DevServer Started on port 8000...');
-    });
-
-    // module.exports = app;
-}
-
-function getCountries(req, resp, next) {
-    var url = "https://api.earth911.com/earth911.getCountries?api_key=4b9fc883188fe866&callback=";
-
-    https.get(url, function (res) {
-        var body = '';
-
-        res.on('data', function (chunk) {
-            body += chunk;
-        });
-
-        res.on('end', function () {
-            var data = JSON.parse(body);
-            resp.json(data);
-        });
-    }).on('error', function (e) {
-        console.log("nani");
-    });
-
-}
 
 function getMaterialsID(query, latitude, longitude, callback) {
+    console.log("1");
     var url = "https://api.earth911.com/earth911.searchMaterials?query=" + query + "&api_key=4b9fc883188fe866&callback=";
     https.get(url, function (res) {
         var body = '';
@@ -77,18 +22,24 @@ function getMaterialsID(query, latitude, longitude, callback) {
             var data = JSON.parse(body);
             if(data["result"].length <= 0) {
                 callback(data);
+                console.log("error 1");
                 return;
+    
             }
             var id = (data["result"][0]["material_id"]);
+            console.log("before location");
             getNearestLocation(id, latitude, longitude, callback);
+            console.log("3");
         });
     }).on('error', function (e) {
-        console.log("nani");
+        console.log(e);
     });
 
 }
 
 function getNearestLocation(id, latitude, longitude, callback) {
+    console.log("2");
+    
     var url = "https://api.earth911.com/earth911.searchLocations?latitude="+latitude+"&longitude="+longitude+"&material_id="+id+"&api_key=4b9fc883188fe866&callback=";
     https.get(url, function (res) {
         var body = '';
@@ -99,34 +50,48 @@ function getNearestLocation(id, latitude, longitude, callback) {
 
         res.on('end', function () {
             var data = JSON.parse(body);
+            console.log("4");
             callback(data);
+            console.log("4.1");
         });
     }).on('error', function (e) {
-        console.log("nani");
+        console.log(e);
     });
 }
 
-function getLocations(req, resp, next) {
-    var url = "https://api.earth911.com/earth911.searchLocations?latitude="+latitude+"&longitude="+longitude+"&material_id="+id+"&api_key=4b9fc883188fe866&callback=";
-    console.log(url);
-    https.get(url, function (res) {
-        var body = '';
 
-        res.on('data', function (chunk) {
-            body += chunk;
+// app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
+
+app.get('/', (req, res) => {
+    console.log("redirect");
+    res.redirect("/index.html");
+});
+// app.get('/endpoint/location', getLocations);
+app.get("/endpoint/query/:latitude/:longitude/:query",function(req, res){
+    var latitude = req.params.latitude;
+    var longitude = req.params.longitude;
+    setTimeout(function() {
+        getMaterialsID(req.params.query, latitude, longitude, function(data) {
+            console.log("back callback");
+            if (data["result"].length > 0){
+                console.log("sed");
+                res.send(data["result"][0]);
+            }
+            else{
+                console.log("stop trying to reuse 353");
+                res.send({});
+            }
         });
+    }, 1);
 
-        res.on('end', function () {
-            var data = JSON.parse(body);
-            resp.json(data["result"]);
-            console.log(data["result"]);
-        });
-    }).on('error', function (e) {
-        console.log("nani");
-    });
+});
 
-}
+app.use(express.static(__dirname + "/public"));
 
-
-
-startServer();
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
